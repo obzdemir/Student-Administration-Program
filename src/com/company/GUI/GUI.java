d5024bad3c7898a9c67939141e0793ec9b19cba9
Ömer Özdemir <ge82vik@tum.de>
2022-05-14 19:10:55 +0200

    added tabbed GUI

--- src/com/company/GUI/GUI.java
+++ src/com/company/GUI/GUI.java
@@ -9,33 +9,34 @@ import java.awt.event.ActionListener;
 
 public class GUI implements ActionListener {
     private JFrame frame;
-    private JPanel panel;
+    private JPanel welcomePanel;
+    private JPanel studentPanel;
+    private JPanel coursePanel;
     private AdministrationProgram program;
 
     private JButton studentButton;
-    private JButton courseButton;
+    private JButton courseListButton;
+    private JButton enrollButton;
+    private JButton addAStudentButton;
 
     public GUI(AdministrationProgram program){
         this.program = program;
         frame = new JFrame();
-        panel = new JPanel();
+        JTabbedPane tabbedPane = new JTabbedPane();
 
-        studentButton = new JButton("Students");
-        studentButton.addActionListener(this);
-        courseButton = new JButton("Courses");
-        courseButton.addActionListener(this);
-        JLabel welcomeLabel = new JLabel("Welcome!");
+        createWelcomePanel();
+        createStudentPanel();
+        createCoursePanel();
 
-        panel.setBorder(BorderFactory.createEmptyBorder(50, 50, 20, 50));
-        panel.setLayout(new GridLayout(0,1));
-        panel.add(welcomeLabel);
-        panel.add(studentButton);
-        panel.add(courseButton);
+        tabbedPane.add("Welcome", welcomePanel);
+        tabbedPane.add("Students", studentPanel);
+        tabbedPane.add("Courses", coursePanel);
 
-        frame.add(panel, BorderLayout.CENTER);
+        frame.add(tabbedPane, BorderLayout.CENTER);
+        frame.setSize(320,200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Student Administration Program");
-        frame.pack();
+        //frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
@@ -44,10 +45,56 @@ public class GUI implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         JButton pressedButton = (JButton) e.getSource();
         if (pressedButton == studentButton){
-            new StudentGUI(program);
+            new StudentListGUI(program);
         }
-        if (pressedButton == courseButton){
+        if (pressedButton == enrollButton){
+            new EnrollSelectStudentGUI(program);
+        }
+        if (pressedButton == addAStudentButton){
+            new AddStudentGUI(program);
+        }
+
+        if (pressedButton == courseListButton){
             new CourseListGUI(program);
         }
     }
+
+    private void createWelcomePanel(){
+        welcomePanel = new JPanel();
+        JLabel welcomeLabel = new JLabel("Welcome!");
+        JLabel descriptionLabel = new JLabel("This is a student administration program.");
+        JLabel enjoyLabel = new JLabel("Enjoy!");
+
+        welcomePanel.setBorder(BorderFactory.createEmptyBorder(20, 30, 10, 30));
+        welcomePanel.setLayout(new GridLayout(0,1));
+        welcomePanel.add(welcomeLabel);
+        welcomePanel.add(descriptionLabel);
+        welcomePanel.add(enjoyLabel);
+    }
+
+    private void createStudentPanel(){
+        studentPanel = new JPanel();
+        studentButton = new JButton("Show all students");
+        studentButton.addActionListener(this);
+        enrollButton = new JButton("enroll a student");
+        enrollButton.addActionListener(this);
+        addAStudentButton = new JButton("add a student");
+        addAStudentButton.addActionListener(this);
+        studentPanel.setBorder(BorderFactory.createEmptyBorder(20, 50, 20, 50));
+        studentPanel.setLayout(new GridLayout(0,1));
+        studentPanel.add(studentButton);
+        studentPanel.add(studentButton);
+        studentPanel.add(enrollButton);
+        studentPanel.add(addAStudentButton);
+    }
+
+    private void createCoursePanel(){
+        coursePanel = new JPanel();
+        courseListButton = new JButton("Show all courses");
+        courseListButton.addActionListener(this);
+        coursePanel.setBorder(BorderFactory.createEmptyBorder(20, 50, 20, 50));
+        coursePanel.setLayout(new GridLayout(0,1));
+        coursePanel.add(courseListButton);
+    }
 }
+
